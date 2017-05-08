3cf004a94f9a51dc44bdd41ddafd15d24f99756d
Adrian <adrian@mxlinux.org>
2017-05-08 14:01:04 -0400

    minor change
    Former-commit-id: 8413d0b35381b53736275942496eff37f3a7bb24 [formerly 93f5e5d8e73fb669816ad6b1bc889c75050a3957]
    Former-commit-id: f04b6c419efb651d113a684d091d969d7f6b1fc9
    Former-commit-id: 49e82c96e00d1b4a71eb71c9b62d28b3e019afd5
    Former-commit-id: 03a59c5e72180893559205e9dd47804a5fac8091
    Former-commit-id: c9e106db0b91e924ee5ed3bb5602662456c2099a

--- mxsnapshot.cpp
+++ mxsnapshot.cpp
@@ -784,9 +784,10 @@ void mxsnapshot::on_buttonAbout_clicked()
                        tr("Program for creating a live-CD from the running system for MX Linux") + "</h3></p><p align=\"center\"><a href=\"http://mxlinux.org\">http://mxlinux.org</a><br /></p><p align=\"center\">" +
                        tr("Copyright (c) MX Linux") + "<br /><br /></p>", 0, this);
     msgBox.addButton(tr("License"), QMessageBox::AcceptRole);
-    msgBox.addButton(tr("Cancel"), QMessageBox::RejectRole);
-    if (msgBox.exec() == QMessageBox::AcceptRole)
+    msgBox.addButton(tr("Cancel"), QMessageBox::NoRole);
+    if (msgBox.exec() == QMessageBox::AcceptRole) {
         system("mx-viewer file:///usr/share/doc/mx-snapshot/license.html '" + tr("MX Snapshot License").toUtf8() + "'");
+    }
     this->show();
 }
 
