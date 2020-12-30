b3223bcf8728ad1cd603bb68e59548d5f9dcd67a
Adrian <adrian@mxlinux.org>
2020-12-30 13:51:39 -0500

    minor reformatting
    Former-commit-id: 0efe68d6f63666302b77f19eebf45977dc2cfd7f [formerly b255e3edc1975b214537159a5f9f09ed179cd57c]
    Former-commit-id: 8370550f33ae7db3751c38b6d2692391ab93e739
    Former-commit-id: c2cd4373f31f87240f6c4c1560eaeaf066d76420
    Former-commit-id: 36485222175b10962eecc4182960295be43c2180
    Former-commit-id: f7e65ce73e597ef33ec89f22b292a47303db9c1b

--- main.cpp
+++ main.cpp
@@ -76,13 +76,9 @@ int main(int argc, char *argv[])
 
     if (getuid() == 0) {
         QString log_name= "/var/log/" + app.applicationName() + ".log";
-        // archive old log
         system("[ -f " + log_name.toUtf8() + " ] && mv " + log_name.toUtf8() + " " + log_name.toUtf8() + ".old");
-        // Set the logging files
         logFile.reset(new QFile(log_name));
-        // Open the file logging
         logFile.data()->open(QFile::Append | QFile::Text);
-        // Set handler
         qInstallMessageHandler(messageHandler);
 
         qDebug().noquote() << app.applicationName() << QObject::tr("version:") << app.applicationVersion();
@@ -97,26 +93,20 @@ int main(int argc, char *argv[])
 // The implementation of the handler
 void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
-    // Write to terminal
     QTextStream term_out(stdout);
-    term_out << msg << endl;
+    term_out << msg << QStringLiteral("\n");
 
-    // Open stream file writes
     QTextStream out(logFile.data());
-
-    // Write the date of recording
     out << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz ");
-    // By type determine to what level belongs message
     switch (type)
     {
-    case QtInfoMsg:     out << "INF "; break;
-    case QtDebugMsg:    out << "DBG "; break;
-    case QtWarningMsg:  out << "WRN "; break;
-    case QtCriticalMsg: out << "CRT "; break;
-    case QtFatalMsg:    out << "FTL "; break;
+    case QtInfoMsg:     out << QStringLiteral("INF "); break;
+    case QtDebugMsg:    out << QStringLiteral("DBG "); break;
+    case QtWarningMsg:  out << QStringLiteral("WRN "); break;
+    case QtCriticalMsg: out << QStringLiteral("CRT "); break;
+    case QtFatalMsg:    out << QStringLiteral("FTL "); break;
     }
-    // Write to the output category of the message and the message itself
-    out << context.category << ": " << msg << endl;
+    out << context.category << QStringLiteral(": ") << msg << endl;
 }
 
 
