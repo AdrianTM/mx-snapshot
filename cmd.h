29dbee3cc7ede606ea5f6178ed22b2db83b4fa89
Adrian <adrian@mxlinux.org>
2022-06-15 00:29:33 -0400

    Remove old definition
    Former-commit-id: 69c9491ca965e7baff0e3219c338a7f8190ace5e [formerly dc46f313523d9d43309abed4c20f0865cec4f8b4]
    Former-commit-id: 8f049c4f6b8a0cac6bd033f637eae1822b8b97a7
    Former-commit-id: 570a17462ce2df96c41dcf41e2d80de1c53941f2
    Former-commit-id: 914416aad47d8ab7bc60ce363e7dd4ad852a0b4c

--- cmd.h
+++ cmd.h
@@ -11,7 +11,6 @@ class Cmd: public QProcess
     Q_OBJECT
 public:
     explicit Cmd(QObject *parent = nullptr);
-    void halt();
     bool run(const QString &cmd, bool quiet = false);
     bool run(const QString &cmd, QString& output, bool quiet = false);
     QString getCmdOut(const QString &cmd, bool quiet = false);
