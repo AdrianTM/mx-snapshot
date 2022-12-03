bf0be6c6380519c3fe4df2f224d766687a60346d
Adrian <adrian@mxlinux.org>
2022-12-03 12:25:31 -0500

    Can use QApplication here, not relevant anyway
    Former-commit-id: 6dae9c6d738fd791c0384a8a92273111b70cbd3b [formerly 4cccbe6f87c3a20cba22f145ed2c2ed417d4c526]
    Former-commit-id: 2474ea5ead07a1b658a317198ebd9820d237e5e9
    Former-commit-id: bac9edb36b7f228edd5157b9047cfab7768fcc18
    Former-commit-id: 8822be1dfa81764608786dfadb5a531842b6dc87

--- main.cpp
+++ main.cpp
@@ -140,7 +140,6 @@ int main(int argc, char *argv[])
         }
 #endif
     QCoreApplication::setApplicationVersion(VERSION);
-    QApplication::setApplicationDisplayName(QObject::tr("MX Snapshot"));
     parser.process(app);
     setTranslation();
     checkSquashfs();
