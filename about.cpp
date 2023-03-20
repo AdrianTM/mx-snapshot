0fb422bda6a7b4c2c3f279edb96e74c028f537db
Adrian <adrian@mxlinux.org>
2023-03-20 00:45:09 -0400

    Make sure GUI is active while displaying help
    Former-commit-id: 5a478f4ec4d34aac7e37d7a54101953395e57516 [formerly 0b4aa1bc42ed81ee30e52c0071730e025a630bd2]
    Former-commit-id: cfe7b8b663e9890f29b179f6ca0e4ead2724b952
    Former-commit-id: cea8632dc8a4e284c88a8a6f9d28229a4e0081db
    Former-commit-id: 56088ef3578f50f8ffe9a59cb49f73c106f62a1a

--- about.cpp
+++ about.cpp
@@ -19,10 +19,10 @@ void displayDoc(const QString &url, const QString &title)
     qputenv("HOME", starting_home.toUtf8());
     // prefer mx-viewer otherwise use xdg-open (use runuser to run that as logname user) "gio open" would also work here
     if (QFile::exists(QStringLiteral("/usr/bin/mx-viewer"))) {
-        QProcess::execute(QStringLiteral("mx-viewer"), {url, title});
+        QProcess::startDetached(QStringLiteral("mx-viewer"), {url, title});
     } else {
         if (getuid() != 0) {
-            QProcess::execute(QStringLiteral("xdg-open"), {url});
+            QProcess::startDetached(QStringLiteral("xdg-open"), {url});
             return;
         } else {
             QProcess proc;
