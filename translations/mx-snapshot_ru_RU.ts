a069f15e116bca453671425e2a0f1d3f0e855031
Adrian <adrian@mxlinux.org>
2023-11-28 00:29:11 -0500

    New build
    Former-commit-id: 7d9936585d7a5af05245729639fb891f156c6da8 [formerly 6046df25b72090b60b06cf065eb8cef40cda9172]
    Former-commit-id: 85310f5533adad26d539245824e2ed7e07000cdf
    Former-commit-id: a139aee69d04cd6f8c7cc492fdc75d6f8140e786
    Former-commit-id: 06626347ab04d552d7129f1d6bc86a4e76d53397

--- translations/mx-snapshot_ru_RU.ts
+++ translations/mx-snapshot_ru_RU.ts
@@ -643,8 +643,8 @@
         <location filename="../main.cpp" line="289"/>
         <location filename="../settings.cpp" line="252"/>
         <location filename="../settings.cpp" line="267"/>
-        <location filename="../settings.cpp" line="750"/>
-        <location filename="../settings.cpp" line="846"/>
+        <location filename="../settings.cpp" line="753"/>
+        <location filename="../settings.cpp" line="849"/>
         <source>Error</source>
         <translation>Ошибка</translation>
     </message>
@@ -736,8 +736,8 @@
         <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../settings.cpp" line="743"/>
-        <location filename="../settings.cpp" line="839"/>
+        <location filename="../settings.cpp" line="746"/>
+        <location filename="../settings.cpp" line="842"/>
         <source>Output file %1 already exists. Please use another file name, or delete the existent file.</source>
         <translation type="unfinished"/>
     </message>
